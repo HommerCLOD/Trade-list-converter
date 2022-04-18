d431040314a88b2be09e2e2ffd404d95a8fc3a99
HommerCLOD <hommerclod@gmail.com>
2022-04-18 21:21:54 +0300

    upload

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const file = require('fs')
 
+function converter() {
+    const data = file.readFileSync("./Unhandled/list.txt", (error, data) => {
+        if (error) throw error
+
+        return data
+    })
+
+    const settings = file.readFileSync("./settings.txt", (error, data) => {
+        if (error) throw error
+
+        return data
+    })
+
+    const parsed_data = JSON.parse(data.toString())
+
+    const parsed_settings = JSON.parse(settings.toString())
+
+    console.log(parsed_settings)
+
+    const handled = []
+
+    for (let element of parsed_data) {
+        handled.push({
+            appid: parsed_settings.appid,
+            game_name: parsed_settings.game_name,
+            hash_name: element
+        })
+    }
+
+    file.writeFileSync("./Handled_Arrays/list.json", JSON.stringify(handled))
+}
+
+converter()
